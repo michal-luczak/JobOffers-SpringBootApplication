ac5ae427885cc80c091844f65bec1caa8251c8fc
Michal <michalluczak4a@gmail.com>
2023-07-12 17:37:15 +0200

    small fix

--- infrastructure/adapters/offer-rest-controller/src/main/java/pl/luczak/michal/joboffersapp/OfferSaveRequest.java
+++ infrastructure/adapters/offer-rest-controller/src/main/java/pl/luczak/michal/joboffersapp/OfferSaveRequest.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+import org.springframework.context.annotation.PropertySource;
 
 @Builder
+@PropertySource("classpath:validationMessages.properties")
 public record OfferSaveRequest(
         @NotNull(message = "{not.null}")
         @NotBlank(message = "{not.blank}")
